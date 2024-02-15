6366a1d8c2d10892eb2b5967ae1c71c0cf7f59d6
tiagomcrodrigues <83243003+tiagomcrodrigues@users.noreply.github.com>
2024-02-15 16:33:48 -0300

    Add files via upload

---
+++ produtos/src/produtos02/produtos02/Data/Entities/Estoque.cs
@@ -0,0 +1,11 @@
+﻿namespace produtos02.Data.Entities
+{
+    public class Estoque
+    { 
+        public Guid ProdutoId { get; set; }
+        public double Quantidade { get; set;}
+        public double CustoMedio { get; set; }
+
+        public virtual Produto Produto { get; set; }
+    }
+}
